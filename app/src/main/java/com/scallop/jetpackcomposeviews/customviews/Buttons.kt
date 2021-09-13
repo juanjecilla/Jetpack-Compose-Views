dcf5ae73267710d7087b3e3719f63d63d13394b2
Juanje Cilla <juanjesus.cillaugarte@axonista.com>
2021-09-13 12:52:58 +0200

    RepeatingButton added

---
+++ app/src/main/java/com/scallop/jetpackcomposeviews/customviews/Buttons.kt
@@ -0,0 +1,94 @@
+package com.scallop.jetpackcomposeviews.customviews
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * https://proandroiddev.com/creating-a-repeating-button-with-jetpack-compose-b39c4f559f7d
+ */
+@Composable
+fun RepeatingButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    maxDelayMillis: Long = 1000,
+    minDelayMillis: Long = 5,
+    delayDecayFactor: Float = .20f,
+    content: @Composable RowScope.() -> Unit
+) {
+    Button(
+        modifier = modifier.repeatingClickable(
+            interactionSource = interactionSource,
+            enabled = enabled,
+            maxDelayMillis = maxDelayMillis,
+            minDelayMillis = minDelayMillis,
+            delayDecayFactor = delayDecayFactor
+        ) { onClick() },
+        onClick = {},
+        enabled = enabled,
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        colors = colors,
+        contentPadding = contentPadding,
+        content = content
+    )
+}
+
+fun Modifier.repeatingClickable(
+    interactionSource: InteractionSource,
+    enabled: Boolean,
+    maxDelayMillis: Long = 1000,
+    minDelayMillis: Long = 5,
+    delayDecayFactor: Float = .20f,
+    onClick: () -> Unit
+): Modifier = composed {
+
+    val currentClickListener by rememberUpdatedState(onClick)
+
+    pointerInput(interactionSource, enabled) {
+        forEachGesture {
+            coroutineScope {
+                awaitPointerEventScope {
+                    val down = awaitFirstDown(requireUnconsumed = false)
+                    val heldButtonJob = launch {
+                        var currentDelayMillis = maxDelayMillis
+                        while (enabled && down.pressed) {
+                            currentClickListener()
+                            delay(currentDelayMillis)
+                            val nextMillis = currentDelayMillis - (currentDelayMillis * delayDecayFactor)
+                            currentDelayMillis = nextMillis.toLong().coerceAtLeast(minDelayMillis)
+                        }
+                    }
+                    waitForUpOrCancellation()
+                    heldButtonJob.cancel()
+                }
+            }
+        }
+    }
+}
