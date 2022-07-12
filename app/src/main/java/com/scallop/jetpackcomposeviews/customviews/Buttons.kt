c6d93e809f4d07ce1708fea638203e1f14f89739
Juanje Cilla <juan.cilla@marinosoftware.com>
2022-07-12 10:01:28 +0200

    Swipe button

--- app/src/main/java/com/scallop/jetpackcomposeviews/customviews/Buttons.kt
+++ app/src/main/java/com/scallop/jetpackcomposeviews/customviews/Buttons.kt
@@ -3,22 +3,52 @@ package com.scallop.jetpackcomposeviews.customviews
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonElevation
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.runtime.*
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
@@ -29,14 +59,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 /**
  * https://proandroiddev.com/creating-a-repeating-button-with-jetpack-compose-b39c4f559f7d
@@ -578,3 +615,82 @@ fun ButtonNoRipple(
 }
 
 // https://semicolonspace.com/android-compose-music-button/
+
+
+//https://barros9.medium.com/custom-view-swipe-button-in-compose-78c8c74e3f9a
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeButton(
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(16.dp),
+    backgroundColor: Color = Color.White,
+    borderStroke: BorderStroke = BorderStroke(2.dp, Color.Black),
+    elevation: Dp = 8.dp,
+    icon: @Composable () -> Unit,
+    text: String,
+    textStyle: TextStyle = TextStyle(Color.Black, 20.sp),
+    onSwipe: () -> Unit
+) {
+    val swipeableState = rememberSwipeableState(initialValue = 0)
+    val textAlpha by animateFloatAsState(
+        if (swipeableState.offset.value > 10f) (1 - swipeableState.progress.fraction) else 1f
+    )
+
+    if (swipeableState.isAnimationRunning) {
+        DisposableEffect(Unit) {
+            onDispose {
+                if (swipeableState.currentValue == 1) {
+                    onSwipe()
+                }
+            }
+        }
+    }
+
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        shape = shape,
+        color = backgroundColor,
+        border = borderStroke,
+        elevation = elevation
+    ) {
+        BoxWithConstraints(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            var iconSize by remember { mutableStateOf(IntSize.Zero) }
+            val maxWidth = with(LocalDensity.current) {
+                this@BoxWithConstraints.maxWidth.toPx() - iconSize.width
+            }
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center),
+                textAlign = TextAlign.End,
+                text = text,
+                style = textStyle.copy(color = textStyle.color.copy(alpha = textAlpha))
+            )
+            Box(
+                modifier = Modifier
+                    .onGloballyPositioned {
+                        iconSize = it.size
+                    }
+                    .swipeable(
+                        state = swipeableState,
+                        anchors = mapOf(
+                            0f to 0,
+                            maxWidth to 1
+                        ),
+                        thresholds = { _, _ -> FractionalThreshold(0.9f) },
+                        orientation = Orientation.Horizontal
+                    )
+                    .offset {
+                        IntOffset(swipeableState.offset.value.roundToInt(), 0)
+                    }
+            ) {
+                icon()
+            }
+        }
+    }
+}
