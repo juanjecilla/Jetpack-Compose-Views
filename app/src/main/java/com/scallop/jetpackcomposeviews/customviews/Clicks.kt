78cb0d2ac157f2fe56b2dc2b41ac40621f621160
Juanje Cilla <juan.cilla@marinosoftware.com>
2022-10-06 17:43:39 +0200

    Refreshing button

--- app/src/main/java/com/scallop/jetpackcomposeviews/customviews/Clicks.kt
+++ app/src/main/java/com/scallop/jetpackcomposeviews/customviews/Clicks.kt
@@ -1,25 +1,49 @@
 package com.scallop.jetpackcomposeviews.customviews
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.repeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 // https://al-e-shevelev.medium.com/how-to-prevent-multiple-clicks-in-android-jetpack-compose-8e62224c9c5e
 
@@ -114,7 +138,7 @@ fun Modifier.pressClickEffect() = composed {
         .clickable(
             interactionSource = remember { MutableInteractionSource() },
             indication = null,
-            onClick = {  }
+            onClick = { }
         )
         .pointerInput(buttonState) {
             awaitPointerEventScope {
@@ -160,4 +184,135 @@ fun Modifier.shakeClickEffect() = composed {
                 }
             }
         }
+}
+
+//https://betterprogramming.pub/exploring-launchedeffect-and-infinitetransition-in-jetpack-compose-5a82ba948a15
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun ButtonRefresh() {
+
+    var buttonState by remember { mutableStateOf(ButtonState2.TEXT) }
+    var isSelected by remember { mutableStateOf(false) }
+    val (scaleText, scaleIcon, displayString) = refershScalingAnimation(buttonState, isSelected)
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(GreenButton.copy(alpha = 0.1f))
+            .height(40.dp)
+            .clickableNoRipple {
+                buttonState = buttonState.getOppositeState()
+                isSelected = !isSelected
+            }
+    ) {
+
+        Text(
+            text = displayString,
+            color = GreenButton,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(start = 12.dp, end = 12.dp)
+                .scale(scaleText.value)
+        )
+
+        Box(
+            modifier = Modifier
+                .width(40.dp)
+                .scale(scaleIcon.value)
+                .rotate(if (buttonState == ButtonState2.TEXT) 0f else rotateComposable()),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "Refresh",
+                tint = GreenButton,
+            )
+        }
+
+    }
+}
+
+val GreenButton = Color(0xFF00914b)
+
+@Composable
+fun refershScalingAnimation(
+    buttonState: ButtonState2,
+    isSelected: Boolean,
+): Triple<Animatable<Float, AnimationVector1D>, Animatable<Float, AnimationVector1D>, String> {
+    val scaleText = remember { Animatable(initialValue = 1f) }
+    val scaleIcon = remember { Animatable(initialValue = 0f) }
+    var displayString by remember { mutableStateOf("Refresh") }
+
+    LaunchedEffect(key1 = isSelected) {
+        if (buttonState == ButtonState2.ICON) {
+            scaleText.animateTo(
+                targetValue = 0f,
+                animationSpec = tween(
+                    durationMillis = 500
+                )
+            )
+            displayString = ""
+            scaleIcon.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(
+                    durationMillis = 500
+                )
+            )
+        } else if (buttonState == ButtonState2.TEXT) {
+
+            scaleIcon.animateTo(
+                targetValue = 0f,
+                animationSpec = tween(
+                    durationMillis = 500
+                )
+            )
+            displayString = "Refresh"
+            scaleText.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(
+                    durationMillis = 500
+                )
+            )
+        }
+    }
+
+    return Triple(scaleText, scaleIcon, displayString)
+}
+
+@Composable
+fun rotateComposable(): Float {
+    val _infiniteTransition = rememberInfiniteTransition()
+    val twinCircleAnimation by _infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    return twinCircleAnimation
+}
+
+enum class ButtonState2 {
+    TEXT, ICON
+}
+
+fun ButtonState2.getOppositeState(): ButtonState2 = when (this) {
+    ButtonState2.TEXT -> ButtonState2.ICON
+    ButtonState2.ICON -> ButtonState2.TEXT
+}
+
+inline fun Modifier.clickableNoRipple(
+    crossinline onClick: () -> Unit,
+): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+        onClick = {
+            onClick()
+        }
+    )
 }
