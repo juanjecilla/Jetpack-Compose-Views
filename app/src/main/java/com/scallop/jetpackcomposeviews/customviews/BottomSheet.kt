884e991a4269abfdc1091b70096ab8aee9f91161
Juanje Cilla <juanje.cilla@gmail.com>
2022-12-11 12:23:02 +0100

    Bottom sheet dialog

--- app/src/main/java/com/scallop/jetpackcomposeviews/customviews/BottomSheet.kt
+++ app/src/main/java/com/scallop/jetpackcomposeviews/customviews/BottomSheet.kt
@@ -1,12 +1,22 @@
 package com.scallop.jetpackcomposeviews.customviews
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.util.Log
 import android.view.ViewGroup
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,8 +25,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.launch
 
@@ -91,6 +104,82 @@ private fun BottomSheetWrapper(
             }
             else -> {
                 Log.i(TAG, "Bottom sheet ${modalBottomSheetState.currentValue} state")
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BottomSheetLayout() {
+    val coroutineScope = rememberCoroutineScope()
+    val modalSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmStateChange = { it != ModalBottomSheetValue.HalfExpanded },
+        skipHalfExpanded = true
+    )
+
+    var isSheetFullScreen by remember { mutableStateOf(false) }
+    val roundedCornerRadius = if (isSheetFullScreen) 0.dp else 12.dp
+    val modifier = if (isSheetFullScreen)
+        Modifier
+            .fillMaxSize()
+    else
+        Modifier.fillMaxWidth()
+
+    BackHandler(modalSheetState.isVisible) {
+        coroutineScope.launch { modalSheetState.hide() }
+    }
+
+    ModalBottomSheetLayout(
+        sheetState = modalSheetState,
+        sheetShape = RoundedCornerShape(
+            topStart = roundedCornerRadius,
+            topEnd = roundedCornerRadius
+        ),
+        sheetContent = {
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Button(
+                    onClick = {
+                        isSheetFullScreen = !isSheetFullScreen
+                    }
+                ) {
+                    Text(text = "Toggle Sheet Fullscreen")
+                }
+
+                Button(
+                    onClick = {
+                        coroutineScope.launch { modalSheetState.hide() }
+                    }
+                ) {
+                    Text(text = "Hide Sheet")
+                }
+            }
+        }
+    ) {
+        Scaffold {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Button(
+                    onClick = {
+                        coroutineScope.launch {
+                            if (modalSheetState.isVisible)
+                                modalSheetState.hide()
+                            else
+                                modalSheetState.animateTo(ModalBottomSheetValue.Expanded)
+                        }
+                    },
+                ) {
+                    Text(text = "Open Sheet")
+                }
             }
         }
     }
