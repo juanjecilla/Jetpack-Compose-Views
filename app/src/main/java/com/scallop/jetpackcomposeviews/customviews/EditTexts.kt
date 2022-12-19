2907a2f6864955e5693044e2032842530ae43c7f
Juanje Cilla <juan.cilla@marinosoftware.com>
2022-12-19 13:56:21 +0100

    Another version of auto size text

--- app/src/main/java/com/scallop/jetpackcomposeviews/customviews/EditTexts.kt
+++ app/src/main/java/com/scallop/jetpackcomposeviews/customviews/EditTexts.kt
@@ -1,13 +1,18 @@
 package com.scallop.jetpackcomposeviews.customviews
 
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFontLoader
+import androidx.compose.ui.text.Paragraph
 import androidx.compose.ui.text.ParagraphIntrinsics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -69,6 +74,52 @@ fun AutoSizeTextField(
                 textAlign = TextAlign.Center
             ),
             singleLine = true,
+        )
+    }
+}
+
+// https://blog.canopas.com/autosizing-textfield-in-jetpack-compose-7a80f0270853
+@Composable
+fun AutoSizableTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit = 32.sp,
+    maxLines: Int = Int.MAX_VALUE,
+    minFontSize: TextUnit,
+    scaleFactor: Float = 0.9f,
+) {
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
+        var nFontSize = fontSize
+
+        val calculateParagraph = @Composable {
+            Paragraph(
+                text = value,
+                style = TextStyle(fontSize = nFontSize),
+                density = LocalDensity.current,
+                resourceLoader = LocalFontLoader.current,
+                maxLines = maxLines,
+                width = with(LocalDensity.current) { maxWidth.toPx() }
+            )
+        }
+
+        var intrinsics = calculateParagraph()
+        with(LocalDensity.current) {
+            while ((intrinsics.height.toDp() > maxHeight || intrinsics.didExceedMaxLines) && nFontSize >= minFontSize) {
+                nFontSize *= scaleFactor
+                intrinsics = calculateParagraph()
+            }
+        }
+
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxSize(),
+            shape = RoundedCornerShape(8.dp),
+            maxLines = maxLines,
+            textStyle = TextStyle(fontSize = nFontSize),
         )
     }
 }
